7bc74ebc3a32152a2254127462ab77cbf9f6a5fe
kinjo yuji <kinjoyuji@gmail.com>
2017-11-27 15:56:28 +0900

    localsをjaに変更

--- ruby/rails-generator.rb
+++ ruby/rails-generator.rb
@@ -69,3 +69,9 @@ end
 # convert erb file to slim
 run 'bundle exec erb2slim -d app/views'
 
+
+# install locales
+remove_file 'config/locales/en.yml'
+run 'wget https://raw.github.com/svenfuchs/rails-i18n/master/rails/locale/en.yml -P config/locales/'
+run 'wget https://raw.github.com/svenfuchs/rails-i18n/master/rails/locale/ja.yml -P config/locales/'
+

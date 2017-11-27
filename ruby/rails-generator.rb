30c4efaf847456513cd3bdeaf2296176143e6996
kinjo yuji <kinjoyuji@gmail.com>
2017-11-27 20:51:54 +0900

    wheneverの設定

--- ruby/rails-generator.rb
+++ ruby/rails-generator.rb
@@ -214,15 +214,18 @@ run "bundle exec guard init rspec"
 # convert erb file to slim
 run 'bundle exec erb2slim -d app/views'
 
+# setting whenever
+run 'bundle exec wheneverize .'
+
 # setting frontend
 front_resorce = ask("choise front-end 'none:1' or 'vue:2' or 'react:3' ")
 case front_resorce
-when 1
+when '1'
   return
-when 2
+when '2'
   run 'bundle exec rails webpacker:install'
   run 'bundle exec rails webpacker:install:vue'
-when 3
+when '3'
   run 'bundle exec rails webpacker:install'
   run 'bundle exec rails webpacker:install:react'
 end

c437d81305b873513827a66e3898be0ad308a09a
kinjo yuji <kinjoyuji@gmail.com>
2018-02-03 19:42:59 +0900

    Internet Gateway構築

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,6 +1,7 @@
 # 参考
 # https://www.terraform.io/docs/providers/aws/
 
+# VPC構築
 resource "aws_vpc" "vpc-1" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
@@ -11,6 +12,7 @@ resource "aws_vpc" "vpc-1" {
   }
 }
 
+# public subnet構築
 resource "aws_subnet" "vpc-1-public-subnet" {
   vpc_id            = "${aws_vpc.vpc-1.id}"
   cidr_block        = "10.0.1.0/24"
@@ -21,6 +23,7 @@ resource "aws_subnet" "vpc-1-public-subnet" {
   }
 }
 
+# private subnet構築
 resource "aws_subnet" "vpc-1-private-subnet" {
   vpc_id            = "${aws_vpc.vpc-1.id}"
   cidr_block        = "10.0.2.0/24"
@@ -28,5 +31,14 @@ resource "aws_subnet" "vpc-1-private-subnet" {
 
   tags {
     Name = "vpc-1-private-subnet"
+  }
+}
+
+# internet gateway構築
+resource "aws_internet_gateway" "vpc-1-igw" {
+  vpc_id = "${aws_vpc.vpc-1.id}"
+
+  tags {
+    Name = "vpc-1-igw"
   }
 }

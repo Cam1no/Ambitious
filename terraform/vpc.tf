6d754623543ee57409d90ce09d15fa56b4c6c7e9
kinjo yuji <kinjoyuji@gmail.com>
2018-02-03 19:03:17 +0900

    private-subnetとpublic-subnetの構築

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -7,3 +7,23 @@ resource "aws_vpc" "vpc-1" {
     Name = "vpc-1"
   }
 }
+
+resource "aws_subnet" "vpc-1-public-subnet" {
+  vpc_id            = "${aws_vpc.vpc-1.id}"
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1a"
+
+  tags {
+    Name = "vpc-1-public-subnet"
+  }
+}
+
+resource "aws_subnet" "vpc-1-private-subnet" {
+  vpc_id            = "${aws_vpc.vpc-1.id}"
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-northeast-1a"
+
+  tags {
+    Name = "vpc-1-private-subnet"
+  }
+}
